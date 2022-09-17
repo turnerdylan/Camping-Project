3d68b4a73b31e72f517aba05db0c49d2801567a9
turnerdylan <dytu4626@colorado.edu>
2022-09-17 14:30:00 -0600

    add start script

--- app.js
+++ app.js
@@ -113,7 +113,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', {err});
 })
 
+const port = process.env.PORT || 3000
 //listen for connections on port 3000
-app.listen(3000, () => {
-    console.log('serving port 3000');
+app.listen(port, () => {
+    console.log(`serving port ${port}`);
 })
